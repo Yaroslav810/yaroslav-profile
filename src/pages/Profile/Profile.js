3fccc69c73233d9e3e6bfa7d8f5c2c936a84c03b
Yaroslav Kuzmin <Yaroslav.Kuzmin.01@yandex.ru>
2021-02-26 10:41:25 +0300

    Testing

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -7,19 +7,19 @@ import Footer from "../../components/Footer/Footer"
 import axios from "axios";
 
 function Profile() {
-  const [data, setData] = React.useState(null);
+  const [data, setData] = React.useState();
   const [loading, setLoading] = React.useState(true);
 
-  // useEffect(() => {
-  //   axios
-  //       .get('/api/get_profile/index.php')
-  //       .then(response => {
-  //         const profileData = response.data;
-  //         setData(profileData);
-  //         console.log(profileData);
-  //         setLoading(false);
-  //       })
-  // }, [setData]);
+  useEffect(() => {
+    axios
+        .get('/api/get_profile/index.php')
+        .then(response => {
+          const profileData = response.data;
+          setData(profileData);
+          console.log(profileData);
+          setLoading(false);
+        })
+  }, [setData]);
 
   if (loading) {
     return (
