b954a5969f2fad494692609894f40d1da9ac27f3
Yaroslav Kuzmin <Yaroslav.Kuzmin.01@yandex.ru>
2021-02-26 23:15:34 +0300

    Fixed background display on mobile devices

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -71,9 +71,6 @@ function Profile() {
           </div>
         </div>
         <Footer />
-        <div className="profile__overlay">
-          <img src="https://c.wallhere.com/photos/a3/43/mountains_high_snow-988481.jpg!d" alt=""/>
-        </div>
       </div>
   );
 }
