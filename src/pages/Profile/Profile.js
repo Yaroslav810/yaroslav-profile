dbb8e06193fc24313e7f67b36804865cc5155e0c
Yaroslav Kuzmin <Yaroslav.Kuzmin.01@yandex.ru>
2021-02-27 00:35:02 +0300

    Filled the page with content

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -6,12 +6,12 @@ import Footer from "../../components/Footer/Footer"
 
 let response = {
   name: 'Ярослав Кузьмин',
-  text: 'Здесь будет что-то написано, Здесь будет что-то написано, ' +
-        'Здесь будет что-то написано, Здесь будет что-то написано, ' +
-        'Здесь будет что-то написано, Здесь будет что-то написано, ' +
-        'Здесь будет что-то написано, Здесь будет что-то написано, ' +
-        'Здесь будет что-то написано, Здесь будет что-то написано, ' +
-        'Здесь будет что-то написано, Здесь будет что-то написано',
+  text: "Всем привет! &#128075;<br /> Меня зовут Ярослав! Я учусь в Волгатехе &#128293; " +
+      "на специальности Программная инженерия. <br /> Эта страница постепенно будет наполняться контентом! Здесь я " +
+      "буду рассказывать про интересные вещи, связанные с IT сферой, а также про интересующие меня темы &#128540; ",
+  about: "Программирование заинтересовало меня ещё в начальной школе &#127890;. Тогда это были интересные рисунки " +
+      "черепашкой в «Логомирах» &#128034;<br /> и ''увлекательные'' соревнования роботов, собранных из «LEGO Mindstorms» &#129302;  С тех пор прошло" +
+      " уже много лет и на данный момент я разрабатываю сайты и web-приложения, использую такие инструменты, как React, Vue, также PHP и Symfony 4.",
   avatar: 'https://psv4.userapi.com/c532036/u188735571/docs/d14/e0757d743a0d/yaroslav.jpg?extra=v2ErZl3slV_any5vFDneaXBo5lKTLVwCjKXhDwchD9RycNRnlcjjdZ454GbxNac5MqrpqQMn2Cqk8jgdkgKA1FPZrz1qap3VQhDf51wzkC1Ypd0V7OfLl6uD3GgUjhsIN6w0RD14CPhGnOITRpRSw63NCg',
   socialNetworks: [
     {
@@ -64,9 +64,8 @@ function Profile() {
               { response.name }
             </div>
             <div className="delimiter" />
-            <div className="profile__about">
-              { response.text }
-            </div>
+            <div className="profile__text" dangerouslySetInnerHTML={{__html: response.text}} />
+            <div className="profile__about" dangerouslySetInnerHTML={{__html: response.about}} />
             <ContactsSection socialNetworks={ response.socialNetworks } />
           </div>
         </div>
